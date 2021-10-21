03f91f91a9979433643bc5962decdcbf9d0c59b1
Tim Davidson <timdavidson2@gmail.com>
2021-10-21 16:29:55 -0400

    grid test

--- portfolio3/src/App.js
+++ portfolio3/src/App.js
@@ -1,11 +1,18 @@
-import { Container, Grid } from "@mui/material";
+import { Container, Grid } from "@material-ui/core";
 import React from "react";
 import "./App.css";
 
 function App() {
   return (
     <Container>
-      <Grid container></Grid>
+      <Grid container>
+        <Grid item xs={12} sm={12} md={4} lg={3}>
+          ABC
+        </Grid>
+        <Grid item xs>
+          xyz
+        </Grid>
+      </Grid>
     </Container>
   );
 }
