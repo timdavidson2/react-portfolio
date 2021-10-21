53e0601d0db4c40789f5e308c213cc3e9ef63a1b
Tim Davidson <timdavidson2@gmail.com>
2021-10-21 10:47:44 -0400

    restart build

---
+++ portfolio3/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Our Resume Clone</header>
+    </div>
+  );
+}
+
+export default App;
