815496eaa685db0593c60c6727a4f76a30d5f4e0
Tim Davidson <timdavidson2@gmail.com>
2021-10-22 16:18:14 -0400

    Update Profile.js

--- portfolio3/src/components/Profile/Profile.js
+++ portfolio3/src/components/Profile/Profile.js
@@ -1,5 +1,7 @@
-import { Typography } from "@mui/material";
 import React from "react";
+import { Typography } from "@mui/material";
+
+import CustomTimline from "../Timeline/Timeline";
 
 import "./Profile.css";
 
@@ -20,7 +22,7 @@ const Profile = () => {
         />
       </figure>
       <div className="profile-information">
-        Insert Timeline
+        <CustomTimline />
         <br />
         <button>My Button</button>
       </div>
