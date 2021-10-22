78a680f4cd1ea7a82feeb8bb76439b445a255de9
Tim Davidson <timdavidson2@gmail.com>
2021-10-22 13:27:36 -0400

    update Profile.js with Typography and div

---
+++ portfolio3/src/components/Profile/Profile.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Typography } from "@material-ui/core";
+
+const Profile = () => {
+  return;
+  <div className="profile container-shadow">
+    <div className="profile-name">
+      <Typography className="name">Timothy</Typography>
+      <Typography className="title">
+        Web Developer / Software Engineer
+      </Typography>
+    </div>
+  </div>;
+};
+
+export default Profile;
--- portfolio3/src/components/Profile/Profile.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Profile = () => {
-  return <div>This is a profile compenent.</div>;
-};
-
-export default Profile;
