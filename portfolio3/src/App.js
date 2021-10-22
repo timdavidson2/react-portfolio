f87b56fe1a27c6dd66746f43a646947ece81da59
Tim Davidson <timdavidson2@gmail.com>
2021-10-22 09:39:58 -0400

    Update App.js with import of components and BrowserRouter

--- portfolio3/src/App.js
+++ portfolio3/src/App.js
@@ -1,5 +1,15 @@
-import { Container, Grid } from "@material-ui/core";
 import React from "react";
+import { Container, Grid } from "@material-ui/core";
+
+import Profile from "./components/Profile/Profile";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Portfolio from "./pages/Portfolio/Portfolio";
+import Resume from "./pages/Resume/Resume";
+import Contact from "./pages/Contact/Contact";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 import "./App.css";
 
 function App() {
