762622c69dad08d684b3ceae83de694b132547b7
Tim Davidson <timdavidson2@gmail.com>
2021-10-31 13:05:59 -0400

    add header text and css

--- portfolio/src/components/Header.js
+++ portfolio/src/components/Header.js
@@ -1,7 +1,29 @@
 import React from "react";
+import Typed from "react-typed";
+import "../styles/Header.css";
 
 const Header = () => {
-  return <div className="header-wrapper"></div>;
+  return (
+    <div className="header-wrapper">
+      <div className="main-info">
+        <h1>full stack web developer / it professional</h1>
+        <Typed
+          className="typed-text"
+          strings={[
+            "HTML5",
+            "CSS3",
+            "JavaScript",
+            "node.js",
+            "Bootstrap",
+            "MERN",
+          ]}
+          typeSpeed={150}
+          backSpeed={75}
+          loop
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Header;
--- portfolio/src/components/Navbar.js
+++ portfolio/src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import "../styles/Navbar.css";
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
