d8b2ca5cd4999433cb3f23ca4f74ec56f8daa8a8
Tim Davidson <timdavidson2@gmail.com>
2021-11-02 01:37:23 -0400

    update experience page

--- portfolio/src/pages/Experience.js
+++ portfolio/src/pages/Experience.js
@@ -60,7 +60,7 @@ const Experience = () => {
   return (
     <>
       <div className="container experience-wrapper">
-        <h1 className="experience-heading">experiece page</h1>
+        <h1 className="experience-heading">experiece</h1>
         <hr />
         <div style={{ width: "100%", height: "100%" }}>
           <Chrono items={items} enableOutline mode="VERTICAL_ALTERNATING" />
