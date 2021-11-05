c68f2040ea0d03c2858de9d729ee04f6a31bd082
Tim Davidson <timdavidson2@gmail.com>
2021-11-05 12:47:20 -0400

    add reat-icons

--- portfolio/src/components/Footer/Footer.js
+++ portfolio/src/components/Footer/Footer.js
@@ -1,27 +1,28 @@
 import React from "react";
-import { GitHub } from "react-share";
+import { FaGithub } from "react-icons/fa";
 import "../../styles/Footer.css";
 
 const Footer = () => {
   return (
     <div className="footer">
       <div className="container">
-        <div className="row">
-          <div className="col-lg-6 col-md-6 col-sm-6">
-            <div className="d-flex mt-3">
-              <p>Montpelier, Virginia</p>
-            </div>
-            <div className="d-flex">
-              <a href="tel:555-555-5555">804.555.5555</a>
-            </div>
-            <div className="d-flex mb-3">
-              <a href="mailto:timdavidson2@gmail.com">timdavidson2@gmail.com</a>
-            </div>
-          </div>
-          <div className="col-lg-5 col-md-5 col-sm-6 align-items-center">
 
+        {/* <div className="row"> */}
+        <div className=" address-text">
+          <div className=" mt-3">
+            <p>Montpelier, Virginia</p>
+          </div>
+          <div className="phone">
+            <a href="tel:555-555-5555">804.555.5555</a>
+          </div>
+          <div className="email mb-3">
+            <a href="mailto:timdavidson2@gmail.com">timdavidson2@gmail.com</a>
           </div>
         </div>
+        <div className="mt-5 icon-image">
+          <FaGithub url={"https://github.com/timdavidson2"} size={36} />
+        </div>
+        {/* </div> */}
       </div>
     </div>
   );
